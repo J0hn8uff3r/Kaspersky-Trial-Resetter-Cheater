9db6e0aef78d3cc32ea502ff42e1fec090b95df9
J0hn8uff3r <bufferoverflow@hotmail.es>
2017-01-05 16:50:00 +0100

    Update KIS 17.0.0.611 Trial Resetter Cheater.bat

--- KIS 17.0.0.611 Trial Resetter Cheater.bat
+++ KIS 17.0.0.611 Trial Resetter Cheater.bat
@@ -10,16 +10,24 @@ del "%tmp%\cmdUAC.vbs"
 exit /b
 :noUAC
 
-msg * Remember that you must uninstall Kaspersky Internet Security before running this script.
+FOR %%I IN ("%ProgramFiles(x86)%\Kaspersky Lab\Kaspersky Internet Security 17.0.0\avp.exe") DO SET avpsize=%%~zI
 
-TYPE NUL >"%ProgramFiles(x86)%\Kaspersky Lab\Kaspersky Internet Security 17.0.0\avp.exe"
+IF [%avpsize%] NEQ [] (
+    msg * Please, remember that you must uninstall Kaspersky Internet Security before running this script.
+) ELSE (
+    TYPE NUL >"%ProgramFiles(x86)%\Kaspersky Lab\Kaspersky Internet Security 17.0.0\avp.exe"
 
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\settings" /v EnableSelfProtection /T REG_DWORD /d 00000000 /F
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\settings\def" /v EnableSelfProtection /T REG_DWORD /d 00000000 /F
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\settings" /v EnableSelfProtection /T REG_DWORD /d 00000000 /F
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\settings\def" /v EnableSelfProtection /T REG_DWORD /d 00000000 /F
 
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductRoot /T REG_SZ /d "C:\\Program Files (x86)\\Kaspersky Lab\\Kaspersky Internet Security 17.0.0" /F
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductType /T REG_SZ /d kis /F
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductVersion /T REG_SZ /d 17.0.0.611 /F
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v DataRoot /T REG_SZ /d "C:\\ProgramData\\Kaspersky Lab\\AVP17.0.0" /F
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductStatus /T REG_SZ /d release /F
-msg * Now you can re-run your Kaspersky Trial Resetter 5.1.0.29 again and re-install KIS.
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductRoot /T REG_SZ /d "C:\\Program Files (x86)\\Kaspersky Lab\\Kaspersky Internet Security 17.0.0" /F
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductType /T REG_SZ /d kis /F
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductVersion /T REG_SZ /d 17.0.0.611 /F
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v DataRoot /T REG_SZ /d "C:\\ProgramData\\Kaspersky Lab\\AVP17.0.0" /F
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\KasperskyLab\AVP17.0.0\environment" /v ProductStatus /T REG_SZ /d release /F
+
+	REG ADD "HKEY_CURRENT_USER\Software\KasperskyLab\AVP17.0.0" /v LastLicenseNotificationTime /T REG_SZ /d 1500000000 /F
+	REG ADD "HKEY_CURRENT_USER\Software\KasperskyLab\AVP17.0.0" /v HidePromo /T REG_SZ /d 1 /F
+
+	msg * Done, now you can re-run your Kaspersky Trial Resetter 5.1.0.29 again and re-install KIS.
+)
